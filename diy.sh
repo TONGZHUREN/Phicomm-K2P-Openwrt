297945fdc86c73db030304f0453d17c518137ba4
TONGZHUREN <32959720+TONGZHUREN@users.noreply.github.com>
2022-05-08 00:34:21 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # Add a feed source
 echo 'src-git echo kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
