a79b3f6c3f224fd6273bef04ee81b3eae1fb009b
TONGZHUREN <32959720+TONGZHUREN@users.noreply.github.com>
2022-05-08 21:42:49 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,7 +9,7 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # Add a feed source
-echo 'src-git echo kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+echo 'src-git kenzok https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 
 # add luci -app-
