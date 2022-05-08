25950c48144524e9601741c5f3b34a8e7ba13744
TONGZHUREN <32959720+TONGZHUREN@users.noreply.github.com>
2022-05-09 01:53:45 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -12,10 +12,10 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 echo 'src-git kenzok https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 
-# add luci -app-
-#git clone https://github.com/MeIsReallyBa/k2p-openwrt-mt7615_5.0.2.0.git package/k2p-openwrt-mt7615_5.0.2.0
-#git clone https://github.com/hanwckf/openwrt-mt7615.git package/openwrt-mt7615
-#git clone https://github.com/destan19/OpenAppFilter.git $packages_path/OpenAppFilter
+#添加软件
+packages_path=package/openwrt-packages
+mkdir -p $packages_path
+git clone https://github.com/destan19/OpenAppFilter.git $packages_path/OpenAppFilter
 
 # 取消默认的主题
 #sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
