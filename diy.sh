033dc2a33af27febcb79e3464732e30bc44ff119
TONGZHUREN <32959720+TONGZHUREN@users.noreply.github.com>
2022-05-09 11:23:22 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,8 +9,8 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # Add a feed source
-echo 'src-git kenzok https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+#echo 'src-git kenzok https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 
 #添加软件
 packages_path=package/openwrt-packages
