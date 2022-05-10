a432a7509e743468caf5e1b8f617da397705a81d
TONGZHUREN <32959720+TONGZHUREN@users.noreply.github.com>
2022-05-10 17:06:56 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -13,9 +13,9 @@ echo 'src-git kenzok https://github.com/kenzok8/openwrt-packages' >> feeds.conf.
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 
 #添加软件
-packages_path=package/openwrt-packages
-mkdir -p $packages_path
-git clone https://github.com/destan19/OpenAppFilter.git $packages_path/OpenAppFilter
+#packages_path=package/openwrt-packages
+#mkdir -p $packages_path
+#git clone https://github.com/destan19/OpenAppFilter.git $packages_path/OpenAppFilter
 
 # 取消默认的主题
 #sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
